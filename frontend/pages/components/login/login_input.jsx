c3a8ff70ed10ccc70fd3289d4eb95ec32f95dbce
peammy1146 <peammy2010@hotmail.com>
2021-10-09 22:47:23 +0700

    login page

---
+++ frontend/pages/components/login/login_input.jsx
@@ -0,0 +1,10 @@
+const LoginInput = ({ title }) => {
+  return (
+    <div className="flex flex-col items-start justify-center">
+      <div>{title}</div>
+      <input className="w-full p-1 my-2" />
+    </div>
+  );
+};
+
+export default LoginInput;
---
+++ frontend/pages/login.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import LoginInput from "./components/login/login_input";
+
+import LogoChula from "../public/logo_chula.png";
+
+const Login = () => {
+  return (
+    <div
+      className="h-screen w-screen bg-cover flex flex-col items-center justify-center"
+      style={{
+        backgroundImage: "url(/background-pictures/login-background.svg)",
+      }}
+    >
+      <Image
+        src={LogoChula}
+        width="282"
+        height="164"
+        placeholder="blur"
+        alt="chula logo"
+      />
+      <div className="mt-14 mb-2 text-2xl">เข้าสู่ระบบลงทะเบียนเรียน</div>
+      <div
+        style={{ backgroundColor: "rgb(255,255,255,0.32)" }}
+        className="w-full max-w-xl min-h-auto rounded-3xl p-6 backdrop-blur-3xl filter"
+      >
+        <LoginInput title="รหัสนิสิต 10 หลัก" />
+        <LoginInput title="รหัสผ่าน CU-NET" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
