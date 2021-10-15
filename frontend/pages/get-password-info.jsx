c4075b312b28f7250f77971c2430d4559e9aec47
peammy1146 <peammy2010@hotmail.com>
2021-10-15 23:00:03 +0700

    fig kaohom bugs

--- frontend/pages/get-password-info.jsx
+++ frontend/pages/get-password-info.jsx
@@ -4,7 +4,6 @@ import Footer from "../components/Footer";
 import Button from "../components/Button";
 import GlassBackButton from "../components/GlassBackButton";
 
-
 const GetPasswordInfo = () => {
   const footerRef = useRef();
   const [height, setHeight] = useState(0);
@@ -16,25 +15,34 @@ const GetPasswordInfo = () => {
     <div
       className="h-screen w-screen bg-cover"
       style={{
-        backgroundImage: "url(/background-pictures/getpasswordinfo-background.svg)",
+        backgroundImage:
+          "url(/background-pictures/getpasswordinfo-background.svg)",
       }}
     >
-
       <div
         style={{
           height: `calc(100vh - ${height}px)`,
         }}
-        
         className="flex flex-col items-center justify-center py-4"
       >
-        <div className="flex flex-col"><GlassBackButton /></div>
-        <div className="mt-8 mb-2 text-3xl">นิสิตใหม่รับรหัสผ่าน CUNET</div>
+        <div className="mt-8 mb-4  relative text-3xl w-full max-w-xl flex flex-row justify-center">
+          <div className="absolute top-0 left-2">
+            <GlassBackButton />
+          </div>
+          <div>นิสิตใหม่รับรหัสผ่าน CUNET</div>
+        </div>
         <div
-          style={{ backgroundColor: "rgb(255,255,255,0.5)" }}
+          style={{
+            background:
+              "linear-gradient(104.92deg, rgba(255, 255, 255, 0.648) 12.72%, rgba(255, 255, 255, 0.32) 92.13%)",
+          }}
           className="relative w-full max-w-xl min-h-auto rounded-3xl p-6 backdrop-blur-3xl filter text-xl flex flex-col justify-center"
         >
           <GetPasswordInfoInput title="เลขประจำตัวนิสิต ( Student ID )" />
-          <GetPasswordInfoInput type="password" title="เลขประจำตัวประชาชน ( Citizen ID )" />
+          <GetPasswordInfoInput
+            type="password"
+            title="เลขประจำตัวประชาชน ( Citizen ID )"
+          />
           <div className="items-center flex flex-row justify-center my-4">
             <Button
               onClick={() => {}}
@@ -44,7 +52,7 @@ const GetPasswordInfo = () => {
                   "linear-gradient(93.72deg, #FB70AA 11.59%, rgba(231, 78, 215, 0.9) 89.1%)",
               }}
             >
-            รับรหัสผ่าน
+              รับรหัสผ่าน
             </Button>
           </div>
         </div>
